71a315d40cba56a84315b38ee52fd16dd9dc5394
milashkinaa <74614689+milashkinaa@users.noreply.github.com>
2023-05-29 01:45:16 +0300

    Add files via upload

---
+++ src/main/java/org/example/home/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.example.home.domain.repository;
+
+import org.example.home.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+    User findByEmail(String email);
+}
