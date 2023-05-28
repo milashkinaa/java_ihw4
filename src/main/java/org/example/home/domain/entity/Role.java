0ac7eec4a1da82dd18ef7c20e09ad35f8ee05d9e
milashkinaa <74614689+milashkinaa@users.noreply.github.com>
2023-05-29 01:42:03 +0300

    Create Role.java

---
+++ src/main/java/org/example/home/domain/entity/Role.java
@@ -0,0 +1,12 @@
+package org.example.home.domain.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    CUSTOMER,
+    CHEF,
+    MANAGER
+}
